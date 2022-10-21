a634964a6d1e74693dd590766c22cac55017dc4c
Subhangi <subhangisingh89@gmail.com>
2022-10-21 09:42:06 +0530

    HELLOW

---
+++ TRAINING3RD YEAR/javaprog.java
@@ -0,0 +1,15 @@
+
+class javaprog{
+public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner Sc=new Scanner(System.in);
+        int arr[]=new int[4];
+        int count=0;
+        System.out.println("enter the array element");
+        for(int i=0;i<4;i++){
+            arr[i]=Sc.nextInt();
+        }
+
+        // your code goes here
+    }
+}
